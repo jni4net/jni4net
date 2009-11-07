f734a346885aa187eda619eed231563cd5382cf8
pavel.savara <pavel.savara@gmail.com>
2009-11-07 22:44:26 +0000

    IDisposable removed from java.lang.Object proxy

--- jni4net.n/src/generated/java/lang/Throwable.generated.cs
+++ jni4net.n/src/generated/java/lang/Throwable.generated.cs
@@ -13,7 +13,7 @@ namespace java.lang {
     
     #region Component Designer generated code 
     [global::net.sf.jni4net.attributes.JavaClassAttribute()]
-    public partial class Throwable : global::java.io.Serializable, global::System.IDisposable {
+    public partial class Throwable : global::java.io.Serializable {
         
         internal static global::java.lang.Class staticClass;
         
