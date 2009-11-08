044208817a1b071827bc24791e190084df292602
pavel.savara <pavel.savara@gmail.com>
2009-11-08 03:15:35 +0000

    old header

--- jni4net.n/src/nio/HeapByteBuffer.cs
+++ jni4net.n/src/nio/HeapByteBuffer.cs
@@ -1,11 +1,27 @@
 ﻿/*
- * @(#)Heap-X-Buffer.java	1.28 03/12/19
+ * Copyright 2000-2008 Sun Microsystems, Inc.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * Copyright 2004 Sun Microsystems, Inc. All rights reserved.
- * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
  */
-
-// -- This file was mechanically generated: Do not edit! -- //
 
 using System;
 using java.lang;
@@ -13,17 +29,9 @@ using java.nio;
 
 namespace net.sf.jni4net.nio
 {
-/**
-
- * A read/write HeapByteBuffer.
-
-
-
-
-
-
- */
-
+    /**
+     * A read/write HeapByteBuffer.
+     */
     internal class HeapByteBuffer : ByteBuffer
     {
         internal HeapByteBuffer(int cap, int lim)
