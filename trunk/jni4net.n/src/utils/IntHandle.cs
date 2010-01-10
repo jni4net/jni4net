20cd189200755c038e38087bea3fe7f9e7998616
pavel.savara <pavel.savara@baf3a992-696c-11de-873c-fb8c264d332a>
2010-01-10 23:40:25 +0000

    license in sources updated

--- trunk/jni4net.n/src/utils/IntHandle.cs
+++ trunk/jni4net.n/src/utils/IntHandle.cs
@@ -25,7 +25,6 @@ using System.Runtime.InteropServices;
 
 namespace net.sf.jni4net.utils
 {
-    //TODO solve this for 64bit platform
     public static class IntHandle
     {
         public static void Free(long ptr)
