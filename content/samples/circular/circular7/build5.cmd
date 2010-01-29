833d2660fc4aecf5c71b462c55426a25ab1e4203
pavel.savara <pavel.savara@gmail.com>
2010-01-29 22:26:52 +0000

    Circular use sample

---
+++ content/samples/circular/circular7/build5.cmd
@@ -0,0 +1,6 @@
+@echo off
+mkdir target\
+copy ..\..\..\lib\*.* target
+Csc.exe /nologo /warn:0 /reference:..\..\..\lib\jni4net.n-0.7.0.0.dll /out:target/circular.dll /target:library src4\j4n\IA.generated.cs src4\j4n\IB.generated.cs src4\j4n\B.generated.cs src3\j4n\B.cs 
+javac -cp ../../../lib/jni4net.j-0.7.0.0.jar -d target/ src4/j4n/IA_.java src4/j4n/IB_.java src4/j4n/B.java src5/j4n/A.java src/j4n/IA.java src/j4n/IB.java
+jar cvf target/circular.jar -C target j4n/IA_.class -C target j4n/IB_.class -C target j4n/B.class -C target j4n/A.class -C target j4n/IA.class -C target j4n/IB.class
