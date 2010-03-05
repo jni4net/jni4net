9c9c54f1f7fa5c32000c1163436de99f65ad7fa7
pavel.savara <pavel.savara@baf3a992-696c-11de-873c-fb8c264d332a>
2010-03-05 22:57:30 +0000

    improving jvm generator

---
+++ branches/proxygen2/jni4net.proxygen2/generators/JVMClassGen.cs
@@ -0,0 +1,11 @@
+﻿namespace net.sf.jni4net.proxygen.generators
+{
+    public class JVMClassGen : JVMBaseGenerator
+    {
+        public override void GenerateType()
+        {
+            base.GenerateType();
+            AddAttribute(tgtType, "net.sf.jni4net.attributes.ClrType");
+        }
+    }
+}
--- branches/proxygen2/jni4net.proxygen2/generators/JVMProxyGen.cs
+++ branches/proxygen2/jni4net.proxygen2/generators/JVMProxyGen.cs
@@ -7,7 +7,9 @@ namespace net.sf.jni4net.proxygen.generators
     {
         public override void GenerateType()
         {
+            base.GenerateType();
             CreateEnvConstructor(tgtType, Known.INJEnv);
+            AddAttribute(tgtType, "net.sf.jni4net.attributes.ClrProxy");
         }
 
 
