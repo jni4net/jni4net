d25d204b23eef636fef8e45fa6c053ed90a29a03
pavel.savara@gmail.com <pavel.savara@gmail.com@baf3a992-696c-11de-873c-fb8c264d332a>
2010-12-08 00:54:48 +0000

    bumped version to 0.8.2

--- trunk/content/samples/helloWorldFromCLR/build.cmd
+++ trunk/content/samples/helloWorldFromCLR/build.cmd
@@ -1,4 +1,4 @@
 @echo off
 mkdir work
 copy ..\..\lib\*.* work
-Csc.exe /nologo /warn:0 /reference:..\..\lib\jni4net.n-0.8.1.0.dll /out:work/helloWorldFromCLR.exe /target:exe Program.cs
+Csc.exe /nologo /warn:0 /reference:..\..\lib\jni4net.n-0.8.2.0.dll /out:work/helloWorldFromCLR.exe /target:exe Program.cs
