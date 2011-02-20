181667ad945b1b6366e2b2fc82242aa5e6566b03
pavel.savara@gmail.com <pavel.savara@gmail.com>
2011-02-20 13:24:56 +0000

    removed jre from drools sample

--- content/samples/drools/jre/build.cmd
+++
@@ -1,9 +0,0 @@
-mkdir target
-mkdir target\classes
-copy ..\..\..\lib\*.* target\
-
-Csc.exe /warn:0 /reference:..\..\..\lib\jni4net.n-0.8.3.0.dll /out:jre.j4n.dll /target:library csharp\java\util\Date.generated.cs
-
-javac -nowarn -d target\classes -sourcepath java -cp "target\jni4net.j-0.8.3.0.jar" "java/java_/util/Date_.java"
-jar cvf jre.j4n.jar  -C target\classes "java_/util/Date_.class" > nul 
-
