d87a00b6e9ebd7a756f1eca78ab6fa65e7fcbf99
pavel.savara@gmail.com <pavel.savara@gmail.com@baf3a992-696c-11de-873c-fb8c264d332a>
2011-07-06 21:46:41 +0000

    added sample to identify installed VM runtimes

---
+++ trunk/content/samples/runtimeSelection/run.cmd
@@ -0,0 +1,34 @@
+@echo off
+
+echo JavaHome-MaxClr.cmd
+call JavaHome-MaxClr.cmd
+
+echo IBM32-Jvm16toClr20.cmd
+call IBM32-Jvm16toClr20.cmd
+
+echo IBM32-Jvm16toClr40.cmd
+call IBM32-Jvm16toClr40.cmd
+
+echo Sun32-Jvm15toClr20.cmd
+call Sun32-Jvm15toClr20.cmd
+
+echo Sun32-Jvm15toClr40.cmd
+call Sun32-Jvm15toClr40.cmd
+
+echo Sun32-Jvm16toClr20.cmd
+call Sun32-Jvm16toClr20.cmd
+
+echo Sun32-Jvm16toClr40.cmd
+call Sun32-Jvm16toClr40.cmd
+
+echo Sun64-Jvm15toClr20.cmd
+call Sun64-Jvm15toClr20.cmd
+
+echo Sun64-Jvm15toClr40.cmd
+call Sun64-Jvm15toClr40.cmd
+
+echo Sun64-Jvm16toClr20.cmd
+call Sun64-Jvm16toClr20.cmd
+
+echo Sun64-Jvm16toClr40.cmd
+call Sun64-Jvm16toClr40.cmd
