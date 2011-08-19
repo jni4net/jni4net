966aadb64adf8882d963aa46c56e9f77e4b64006
pavel.savara@gmail.com <pavel.savara@gmail.com@baf3a992-696c-11de-873c-fb8c264d332a>
2011-08-19 20:27:04 +0000

    bumped version to 0.8.5.1

--- trunk/content/samples/circular/circular2/build1.cmd
+++ trunk/content/samples/circular/circular2/build1.cmd
@@ -1,3 +1,3 @@
 @echo off
 mkdir target\
-javac -cp ../../../lib/jni4net.j-0.8.5.0.jar -d target/ src/j4n/IA.java src/j4n/IB.java
+javac -cp ../../../lib/jni4net.j-0.8.5.1.jar -d target/ src/j4n/IA.java src/j4n/IB.java
