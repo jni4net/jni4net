39f674a5ed82de8344c6a9df20b78856f2b78378
Pavel Savara <pavel.savara@gmail.com>
2014-09-23 22:58:29 +0200

    MIT license to source code of runtime

--- jni4net.j/src/main/java/system/Exception.java
+++ jni4net.j/src/main/java/system/Exception.java
@@ -1,20 +1,9 @@
-	/* Copyright (C) 2009 by Pavel Savara
-	This file is part of jni4net library - bridge between Java and .NET
-	http://jni4net.sourceforge.net/
+/* Copyright (C) 2010 by Pavel Savara
+This file is part of jni4net library - bridge between Java and .NET
+http://jni4net.sourceforge.net/
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Lesser General Public License as
-	published by the Free Software Foundation, either version 3
-	of the License, or (at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Lesser General Public License for more details.
-
-	You should have received a copy of the GNU Lesser General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-	*/
+This content is released under the (http://opensource.org/licenses/MIT) MIT License, see license/jni4net-MIT.txt.
+*/
 
 package system;
 
--- jni4net.n/src/BridgeSetup.cs
+++ jni4net.n/src/BridgeSetup.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿#region Copyright (C) 2009 by Pavel Savara
+
+/*
+This file is part of jni4net library - bridge between Java and .NET
+http://jni4net.sourceforge.net/
+
+This content is released under the (http://opensource.org/licenses/MIT) MIT License, see license/jni4net-MIT.txt.
+*/
+
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Security.Permissions;
